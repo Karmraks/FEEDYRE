3f80d89e0ed17caef844cb0628caa8274d1488d9
Karmraks <karmraks@proton.me>
2023-12-31 15:42:41 +0100

    Added basic register form

--- FEEDYRE.Ng/src/app/register/register.component.ts
+++ FEEDYRE.Ng/src/app/register/register.component.ts
@@ -1,12 +1,22 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from "@angular/material/card";
+import { MatFormFieldModule } from "@angular/material/form-field";
+import { MatInputModule } from "@angular/material/input";
 
 @Component({
   selector: 'app-register',
   standalone: true,
-  imports: [],
+  imports: [MatCardModule, MatFormFieldModule, FormsModule, MatButtonModule, MatInputModule],
   templateUrl: './register.component.html',
   styleUrl: './register.component.scss'
 })
 export class RegisterComponent {
 
+  public name: string = '';
+
+  onRegister() {
+    console.log(this.name);
+  }
 }
